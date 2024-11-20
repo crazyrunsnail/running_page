dbe2992e11b2f5927de0f51a86699e270eac391f
crazyrunsnail <fjyuan1991@163.com>
2024-11-20 17:22:54 +0800

    github action and metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -13,15 +13,15 @@ const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
   siteUrl: 'https://yihong.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'Person running page',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://github.com/crazyrunsnail',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/crazyrunsnail',
     },
   ],
 };
